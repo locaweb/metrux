e43d20309b1b903592d2b569eaa06576b56190bf
Ricardo Tealdi <ricardo.tealdi@locaweb.com.br>
2016-06-21 12:21:03 -0300

    Create config builder for fetching logger conf

---
+++ lib/metrux/config_builders/logger.rb
@@ -0,0 +1,44 @@
+module Metrux
+  module ConfigBuilders
+    class Logger
+      LOG_FILE_KEY = 'METRUX_LOG_FILE'.freeze
+      LOG_LEVEL_KEY = 'METRUX_LOG_LEVEL'.freeze
+
+      DEFAULT_LOG_PATH = STDOUT
+      DEFAULT_LOG_LEVEL = :info
+
+      def initialize(yaml)
+        @yaml = yaml
+      end
+
+      def build
+        ::Logger.new(log_file).tap do |logger|
+          logger.level = log_level
+          logger.formatter = ::Logger::Formatter.new
+        end
+      rescue => e
+        Kernel.warn(
+          '[WARNING] Cound\'t configure Metrux\'s logger. '\
+          "#{e.class}: #{e.message}"
+        )
+        nil
+      end
+
+      private
+
+      attr_reader :yaml
+
+      def log_file
+        ENV[LOG_FILE_KEY] || yaml[:log_file] || DEFAULT_LOG_PATH
+      end
+
+      def log_level
+        ::Logger.const_get(
+          (
+            ENV[LOG_LEVEL_KEY] || yaml[:log_level] || DEFAULT_LOG_LEVEL
+          ).to_s.upcase
+        )
+      end
+    end
+  end
+end
