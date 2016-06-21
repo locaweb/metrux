57b499240ef6badebfe153384c3584f1a920fabb
Ricardo Tealdi <ricardo.tealdi@locaweb.com.br>
2016-06-21 11:56:32 -0300

    Add spec configurations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,17 @@
+ENV['RAILS_ENV'] ||= 'test'
+ENV['METRUX_ACTIVE'] = 'true'
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+if ENV['COVERAGE']
+  require 'simplecov'
+  require 'simplecov-rcov'
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  SimpleCov.start do
+    add_filter '/vendor/'
+    add_filter '/spec/'
+  end
+end
+
+require 'pry'
 require 'metrux'
