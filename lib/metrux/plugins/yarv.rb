d5f227ace379f331a01a0acb56f8c8040a0cdc76
Ricardo Tealdi <ricardo.tealdi@locaweb.com.br>
2016-07-08 15:32:49 -0300

    Change Yarv plugin to register only one gauge with all info

--- lib/metrux/plugins/yarv.rb
+++ lib/metrux/plugins/yarv.rb
@@ -2,13 +2,22 @@ module Metrux
   module Plugins
     class Yarv < Base
       def call
-        register('RubyVM#global_method_state') do
-          ::RubyVM.stat[:global_method_state]
+        register('rubyvm') do
+          {
+            global_method_state: global_method_state,
+            global_constant_state: global_constant_state
+          }
         end
+      end
 
-        register('RubyVM#global_constant_state') do
-          ::RubyVM.stat[:global_constant_state]
-        end
+      private
+
+      def global_method_state
+        ::RubyVM.stat[:global_method_state]
+      end
+
+      def global_constant_state
+        ::RubyVM.stat[:global_constant_state]
       end
     end
   end
--- spec/metrux/plugins/yarv_spec.rb
+++ spec/metrux/plugins/yarv_spec.rb
@@ -17,51 +17,35 @@ describe Metrux::Plugins::Yarv do
     let(:global_method_state) { 42 }
     let(:global_constant_state) { 43 }
 
+    let(:expected_result) do
+      {
+        global_method_state: global_method_state,
+        global_constant_state: global_constant_state
+      }
+    end
+
     before do
       @result = nil
 
-      allow(Metrux).to receive(:periodic_gauge)
+      allow(Metrux).to receive(:periodic_gauge) do |*_, &blk|
+        @result = blk.call
+      end
+
       allow(::RubyVM).to receive(:stat).and_return(vm_stat)
     end
 
     it do
-      allow(Metrux)
-        .to receive(:periodic_gauge)
-        .with('RubyVM#global_method_state', anything) do |*_, &blk|
-        @result = blk.call
-      end
-
       call
 
-      expect(@result).to be(global_method_state)
+      expect(@result).to eq(expected_result)
     end
 
     it do
-      allow(Metrux)
+      expect(Metrux)
         .to receive(:periodic_gauge)
-        .with('RubyVM#global_constant_state', anything) do |*_, &blk|
-        @result = blk.call
-      end
+        .with('rubyvm', options)
 
       call
-
-      expect(@result).to be(global_constant_state)
-    end
-
-    %w(RubyVM#global_method_state RubyVM#global_constant_state).each do |key|
-      context "for key #{key}" do
-        let(:expected_key) { key }
-
-        before { allow(Metrux).to receive(:periodic_gauge) }
-
-        it do
-          expect(Metrux)
-            .to receive(:periodic_gauge)
-            .with(expected_key, options)
-
-          call
-        end
-      end
     end
   end
 end
